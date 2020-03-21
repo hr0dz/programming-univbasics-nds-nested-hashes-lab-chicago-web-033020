2448327ba79019b49a2e94770df8fa218335cf39
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-21 20:03:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,6 +19,7 @@ new_array = [
   [PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }],
   [KATE_G = { name:  "Kate Gompert", occupation: "None" }],
   [BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+  ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
