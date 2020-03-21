71002e66123c95efb4ab7f0df87c75a280fdf110
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-21 20:03:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-array = [
+new_array = [
   [DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }],
   [JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }],
   [PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }],
